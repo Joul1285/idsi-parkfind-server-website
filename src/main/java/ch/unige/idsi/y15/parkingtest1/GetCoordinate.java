80d9782abfc043fa7388950c70efd52a98068352
Julien Burn <joul1285@gmail.com>
2015-05-12 13:28:09 +0200

    GetCoordinate class: Add: add a type in GetCoordinate url parameter (not use for now) fix: get parkingList - 1 to avoid a empty entry
    Parser class:
    modif: change path to save kmz file
    modif: change methode to extract kmz file directly without extracting kml file

--- src/main/java/ch/unige/idsi/y15/parkingtest1/GetCoordinate.java
+++ src/main/java/ch/unige/idsi/y15/parkingtest1/GetCoordinate.java
@@ -29,39 +29,45 @@ public class GetCoordinate extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
+	 * 
+	 *      url exemple: /GetCoordinate?lat=46.1948892&long=6.1398835
+	 * 
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		//Get parameters in URL (latitude and longitude)
+
+		// Get parameters in URL (latitude and longitude)
 		double getLatitude = Double.parseDouble(request.getParameter("lat"));
 		double getLongitude = Double.parseDouble(request.getParameter("long"));
-		
+		String getType = request.getParameter("type");
+
 		System.out.println(getLatitude);
 		System.out.println(getLongitude);
-		
-		//Get all object parkingHandi in List
-		List<ParkingHandi> parkingList = ParkingHandi.findAllParkingHandis();
-		
-		System.out.println(Arrays.toString(gps2m(getLatitude, getLongitude, parkingList)));
-		String[] result_shortest = gps2m(getLatitude, getLongitude, parkingList);
-		System.out.println(result_shortest[0]);
-		
-		//Print XML response
-		response.setContentType("application/xml");
-        PrintWriter writer = response.getWriter();
-        writer.println("<?xml version=\"1.0\"?>");
-        writer.println("<parking>");
-        writer.println("<place name='"+result_shortest[0]+"'>");
-        writer.println("<type>handi</type>");
-        writer.println("<latitude>"+result_shortest[1]+"</latitude>");
-        writer.println("<longitude>"+result_shortest[2]+"</longitude>");
-        
-        writer.println("</place>");
-        writer.println("</parking>");
-        writer.flush();
+		System.out.println(getType);
 
+		// Get all object parkingHandi in List
+		List<ParkingHandi> parkingList = ParkingHandi.findAllParkingHandis();
+		parkingList.remove(parkingList.size() - 1); //remove last object (empty)
+
+		// System.out.println(Arrays.toString(gps2m(getLatitude, getLongitude,
+		// parkingList)));
+		String[] result_shortest = gps2m(getLatitude, getLongitude, parkingList);
+		// System.out.println("shortest location is: " + result_shortest[0]);
+
+		// Print XML response
+		response.setContentType("application/xml");
+		PrintWriter writer = response.getWriter();
+		writer.println("<?xml version=\"1.0\"?>");
+		writer.println("<parking>");
+		writer.println("<place name='" + result_shortest[0] + "'>");
+		writer.println("<type>handi</type>");
+		writer.println("<latitude>" + result_shortest[1] + "</latitude>");
+		writer.println("<longitude>" + result_shortest[2] + "</longitude>");
+
+		writer.println("</place>");
+		writer.println("</parking>");
+		writer.flush();
 
 	}
 
@@ -71,25 +77,25 @@ public class GetCoordinate extends HttpServlet {
 	 * @param parkingList
 	 * @return
 	 */
-	private static String[] gps2m(double lat_origin, double lng_origin, List<ParkingHandi> parkingList) {
+	private static String[] gps2m(double lat_origin, double lng_origin,
+			List<ParkingHandi> parkingList) {
 		double pk = (double) (180 / 3.14169);
 
 		double a1 = lat_origin / pk;
 		double a2 = lng_origin / pk;
 		double shortest = 10000.0;
-		
+
 		String short_name = new String();
 		String short_lat = new String();
 		String short_long = new String();
-		
-		
 
 		// Create an iterator to parse all Parking Object
-		Iterator iterator = parkingList.iterator();
-		while (iterator.hasNext()) {
+		// Iterator<ParkingHandi> iterator = parkingList.iterator();
+		for (Iterator<ParkingHandi> iterator = parkingList.iterator(); iterator
+				.hasNext();) {
 			ParkingHandi element = (ParkingHandi) iterator.next();
 
-			//Calculate distance between origin point and all other points
+			// Calculate distance between origin point and all other points
 			double b1 = Double.parseDouble(element.getLatitude()) / pk;
 			double b2 = Double.parseDouble(element.getLongitude()) / pk;
 			double t1 = (double) Math.cos(a1) * (double) Math.cos(a2)
@@ -98,9 +104,9 @@ public class GetCoordinate extends HttpServlet {
 					* (double) Math.cos(b1) * (double) Math.sin(b2);
 			double t3 = (double) Math.sin(a1) * (double) Math.sin(b1);
 			double tt = Math.acos(t1 + t2 + t3) * 6366000;
-			
-			//If next point is shortest than older shortest point, keep it
-			if (tt < shortest){
+
+			// If next point is shortest than older shortest point, keep it
+			if (tt < shortest) {
 				System.out.println(tt);
 				System.out.println(element.getName());
 				shortest = tt;
@@ -109,8 +115,10 @@ public class GetCoordinate extends HttpServlet {
 				short_long = element.getLongitude();
 			}
 		}
-		//Create a array with data of the shortest point and return it
-		String[] result_shortest = {short_name, short_lat, short_long};
+		// Create a array with data of the shortest point and return it
+		System.out.println("shortest location is: "+short_name);
+		String[] result_shortest = { short_name, short_lat, short_long };
+
 		return result_shortest;
 	}
 
--- src/main/java/ch/unige/idsi/y15/parkingtest1/Parser.java
+++ src/main/java/ch/unige/idsi/y15/parkingtest1/Parser.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -37,108 +38,107 @@ import de.micromata.opengis.kml.v_2_2_0.Point;
  */
 public class Parser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Parser() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Parser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 		System.out.println("process start...");
-		
-//		URL u = new URL("http://ge.ch/ags1/rest/services/Mobilite/MapServer/33/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=1%3D1&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=&f=kmz");
-//		URLConnection uc = u.openConnection();
-//		uc.connect();
-//		InputStream in = uc.getInputStream();
-//		FileOutputStream out = new FileOutputStream("test.kmz");
-//		final int BUF_SIZE = 1 << 8;
-//		byte[] buffer = new byte[BUF_SIZE];
-//		int bytesRead = -1;
-//		while((bytesRead = in.read(buffer)) > -1) {
-//		    out.write(buffer, 0, bytesRead);
-//		}
-//		in.close();
-//		out.close();
-//		System.out.println("process end");
-		
-		final Kml kml = Kml.unmarshal(new File("doc.kml"));
+		// File dir1 = new File (".");
+		// System.out.println ("Current dir : " + dir1.getCanonicalPath());
+
+		// Get KMZ file from openGeneva and save it
+		URL u = new URL(
+				"http://ge.ch/ags1/rest/services/Mobilite/MapServer/33/query?text=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&objectIds=&where=1%3D1&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&maxAllowableOffset=&outSR=&outFields=&f=kmz");
+		String fileName = "ParkingHandi";
+
+		// Call function to get the KMZ file, save it and parse it
+		getKMZ(u, fileName);
+
+		System.out.println("process end");
+	}
+
+	public static void getKMZ(URL u, String fileName) throws IOException {
+		URLConnection uc = u.openConnection();
+		uc.connect();
+		InputStream in = uc.getInputStream();
+		File file = new File(fileName + ".kmz");
+		OutputStream out = new FileOutputStream(file);
+
+		final int BUF_SIZE = 1 << 8;
+		byte[] buffer = new byte[BUF_SIZE];
+		int bytesRead = -1;
+		while ((bytesRead = in.read(buffer)) > -1) {
+			out.write(buffer, 0, bytesRead);
+		}
+		in.close();
+		out.close();
+
+		// Open and parse KML file
+		final Kml[] kml = Kml.unmarshalFromKmz(file);
 		System.out.println("process run");
-		// System.out.println("process run2");
-		final Document document = (Document) kml.getFeature();
+		final Document document = (Document) kml[0].getFeature();
 		System.out.println(document.getName());
 
 		List<Feature> t = document.getFeature();
-        for(Object o : t){
-            Folder f = (Folder)o;
-            List<Feature> tg = f.getFeature();
-            for(Object ftg : tg){
-                Placemark g = (Placemark) ftg;
-                System.out.println(g.getName());
-                
-//                //create new object parkinghandi
-//                ParkingHandi test = new ParkingHandi();
-//                test.setName(g.getName());
-//                request.setAttribute("ParkingHandi", test);
-                
-                
-                
-                Point point = (Point) g.getGeometry();
-                List<Coordinate> coordinates = point.getCoordinates(); 
-                for (Coordinate c : coordinates) 
-                { 
-                    System.out.println(c.getLatitude()); 
-                    System.out.println(c.getLongitude()); 
-                    
-                    System.out.println("load loader function");    
-                    onApplicationEvent(g.getName(), String.valueOf(c.getLatitude()), String.valueOf(c.getLongitude()));
-                }  
-                
-                
-            }
-            
-//            this.getServletContext()
-//            	.getRequestDispatcher("/WEB-INF/views/index.jpsx")
-//            	.forward(request, response);
-        }
-	   
-    
-	}
-	
-//	void ParseKMZ(){
-//		final Kml kml = Kml.unmarshal(new File("test.kmz"));
-//		final Placemark placemark = (Placemark) kml.getFeature();
-//		Point point = (Point) placemark.getGeometry();
-//		List<Coordinate> coordinates = point.getCoordinates();
-//		for (Coordinate coordinate : coordinates) {
-//			System.out.println(coordinate.getLatitude());
-//			System.out.println(coordinate.getLongitude());
-//			System.out.println(coordinate.getAltitude());
-//		}
-//	}
+		for (Object o : t) {
+			Folder f = (Folder) o;
+			List<Feature> tg = f.getFeature();
+			for (Object ftg : tg) {
 
-    @Transactional
-    public void onApplicationEvent(String name, String latitude, String longitude) {
-        System.out.println("before: " + ParkingHandi.countParkingHandis());
-        ParkingHandi parkingHandi = new ParkingHandi();
-        parkingHandi.persist();
-        parkingHandi.setName(name);
-        parkingHandi.setLatitude(latitude);
-        parkingHandi.setLongitude(longitude);
-        
-        System.out.println("after: " + ParkingHandi.countParkingHandis());
-    }
-	
+				Placemark g = (Placemark) ftg;
+				System.out.println(g.getName());
+				if (g.getName() != null) {
+					Point point = (Point) g.getGeometry();
+					List<Coordinate> coordinates = point.getCoordinates();
+					for (Coordinate c : coordinates) {
+
+						System.out.println(c.getLatitude());
+						System.out.println(c.getLongitude());
+
+						System.out.println("load loader function");
+						onApplicationEvent(g.getName(),
+								String.valueOf(c.getLatitude()),
+								String.valueOf(c.getLongitude()));
+					}
+
+				}
+
+			}
+		}
+	}
+
+	@Transactional
+	public static void onApplicationEvent(String name, String latitude,
+			String longitude) {
+		System.out.println("before: " + ParkingHandi.countParkingHandis());
+		ParkingHandi parkingHandi = new ParkingHandi();
+		parkingHandi.persist();
+		parkingHandi.setName(name);
+		parkingHandi.setLatitude(latitude);
+		parkingHandi.setLongitude(longitude);
+
+		System.out.println("after: " + ParkingHandi.countParkingHandis());
+	}
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
