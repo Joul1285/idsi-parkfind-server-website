cd4c82f73b217c926019c7603163404567af41b6
Julien Burn <joul1285@gmail.com>
2015-05-21 14:34:05 +0200

    Add javadoc to project Remove old test class

--- src/test/java/ch/unige/idsi/y15/parkingtest1/ParkingHandiDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package ch.unige.idsi.y15.parkingtest1;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-import ch.unige.idsi.y15.parkfind_roo.ParkingHandi;
-
-@RooDataOnDemand(entity = ParkingHandi.class)
-public class ParkingHandiDataOnDemand {
-}
